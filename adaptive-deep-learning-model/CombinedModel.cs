3774559676fc1545dc74d1f46639161e04f6ead2
Mateusz <mateusz.wolnica@gmail.com>
2025-05-07 19:29:31 +0200

    .

---
+++ adaptive-deep-learning-model/CombinedModel.cs
@@ -0,0 +1,39 @@
+﻿using static TorchSharp.torch;
+
+namespace adaptive_deep_learning_model
+{
+    // merge models
+    public class CombinedModel : nn.Module<Tensor, Tensor>
+    {
+        private nn.Module<Tensor, Tensor>[] models;
+
+        public CombinedModel(params nn.Module<Tensor, Tensor>[] models) : base("CombinedModel")
+        {
+            this.models = models;
+            RegisterComponents();
+        }
+
+        public override Tensor? forward(Tensor input)
+        {
+            if(models.Length == 0)
+            {
+                return null;
+            }
+            Tensor[] outputs = new Tensor[models.Length];
+            int index = 0;
+            foreach (var model in models)
+            {
+                outputs[index] = model.forward(input);
+                index++;
+            }
+
+            Tensor final = outputs[0];
+            foreach (var t in outputs.Skip(1))
+            {
+                final = final + t;
+            }
+            var yy = nn.functional.relu(final / models.Length);
+            return yy;
+        }
+    }
+}
--- adaptive-deep-learning-model/Trivial.cs
+++ adaptive-deep-learning-model/Trivial.cs
@@ -3,40 +3,6 @@ using TorchSharp;
 
 namespace adaptive_deep_learning_model
 {
-    // merge models
-    public class CombinedModel : nn.Module<Tensor, Tensor>
-    {
-        private nn.Module<Tensor, Tensor>[] models;
-
-        public CombinedModel(params nn.Module<Tensor, Tensor>[] models) : base("CombinedModel")
-        {
-            this.models = models;
-            RegisterComponents();
-        }
-
-        public override Tensor? forward(Tensor input)
-        {
-            if(models.Length == 0)
-            {
-                return null;
-            }
-            Tensor[] outputs = new Tensor[models.Length];
-            int index = 0;
-            foreach (var model in models)
-            {
-                outputs[index] = model.forward(input);
-                index++;
-            }
-
-            Tensor final = outputs[0];
-            foreach (var t in outputs.Skip(1))
-            {
-                final = final + t;
-            }
-            var yy = nn.functional.relu(final / models.Length);
-            return yy;
-        }
-    }
 
     // probably to be replaced by a RNN or CNN model
     // TODOs: https://pytorch.org/blog/computational-graphs-constructed-in-pytorch/
@@ -137,12 +103,13 @@ namespace adaptive_deep_learning_model
             // to be customized / adaptive
             var EPOCHS = 3;
             var finalLoss = 0.0f;
+            var steps = 1000;
             // to be customized / adaptive
             var optimizer = torch.optim.Adam(this.parameters());
 
             for (int e = 0; e < EPOCHS; e++)
             {
-                for (int i = 0; i < 1000; i++)
+                for (int i = 0; i < steps; i++)
                 {
                     // Compute the loss
                     using var output = loss.forward(this.forward(dataBatch), resultBatch);
