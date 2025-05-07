194cbc599144f4279b1718eb6d7fbb37772d5249
Mateusz <mateusz.wolnica@gmail.com>
2025-05-07 12:04:42 +0200

    .

---
+++ Application/CQRS/Queries/GetPiecesQuery.cs
@@ -0,0 +1,11 @@
+﻿using BuildingBlocks.Application.CQRS;
+
+namespace Card.Application.CQRS.Queries;
+
+public class GetPiecesQuery
+    : IQuery<GetPiecesResult>
+{
+    public GetPiecesQuery()
+    {
+    }
+}
---
+++ Application/CQRS/Queries/GetPiecesResult.cs
@@ -0,0 +1,17 @@
+﻿using Card.Application.Dtos;
+
+namespace Card.Application.CQRS.Queries;
+
+public class GetPiecesResult
+{
+    public GetPiecesResult()
+    {
+        //intentionally left blank
+    }
+    public GetPiecesResult(ModelDto[] model)
+    {
+        Models = model;
+    }
+
+    public ModelDto[] Models { get; set; }
+}
