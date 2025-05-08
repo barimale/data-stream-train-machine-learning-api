d41930c9e6f2e24986609404732762f64918956d
Mateusz <mateusz.wolnica@gmail.com>
2025-05-08 20:20:24 +0200

    .

--- SlowTrainMachineLearningAPI/input_generator.py
+++ SlowTrainMachineLearningAPI/input_generator.py
@@ -6,12 +6,12 @@ import requests
 
 # Example NumPy array
 data = np.array([1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
-
+x = range(100)
 # API endpoint
 api_url = "https://localhost:7174/NeuralNetwork/TrainNetwork"
 
 # Iterate over the NumPy array and make POST requests
-for entry in data:
+for entry in x:
     payload = {"xs": "1,2,3,4,5" , "ys": "1,1,1,1,1,1,1,1,1,1"}  # Convert entry to a format suitable for the API
     try:
         response = requests.post(api_url, headers={"Content-Type": "application/json"},verify=False, json=payload)
