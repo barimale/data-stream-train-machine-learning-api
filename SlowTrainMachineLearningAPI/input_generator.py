dbac434d9f9b3b49586697d3548115c3acde158c
Mateusz <mateusz.wolnica@gmail.com>
2025-05-09 17:16:17 +0200

    .

--- SlowTrainMachineLearningAPI/input_generator.py
+++ SlowTrainMachineLearningAPI/input_generator.py
@@ -12,13 +12,17 @@ api_url = "https://localhost:7174/NeuralNetwork/TrainNetwork"
 
 # Iterate over the NumPy array and make POST requests
 for entry in x:
-    xs = np.random.randn(1,5)
+    xs = np.random.randn(1,5)*100
+    vectorized_floatXs = np.vectorize(int)
+    xs = vectorized_floatXs(xs)
     resultXs = np.array2string(xs, separator=',', suppress_small=True)[1:-1].replace(' ', '')
     resultXs = resultXs.strip("[")
     resultXs = resultXs.strip("]")
     resultXs = resultXs.strip()
     print(resultXs)
-    ys = np.random.randn(1,10)
+    ys = np.random.randn(1,10)*100
+    vectorized_floatYs = np.vectorize(int)
+    ys = vectorized_floatYs(ys)
     resultYs = np.array2string(ys, separator=',', suppress_small=True)[1:-1].replace(' ', '').replace('\n', '')
     resultYs = resultYs.strip("[")
     resultYs = resultYs.strip("]")
