91c5f5170b23826b1a02489e057ebb2f235415c7
Mateusz <mateusz.wolnica@gmail.com>
2025-05-10 14:32:46 +0200

    .

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -18,9 +18,9 @@ public static class DependencyInjection
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
-            //config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-            //config.AddOpenBehavior(typeof(TransactionBehavior<,>));
+            config.AddOpenBehavior(typeof(TransactionBehavior<,>));
         });
 
         services.AddFeatureManagement();
