826200bd21b7c9c4de91903fc6b1004041c99e4e
Mateusz <mateusz.wolnica@gmail.com>
2025-08-03 20:30:18 +0200

    .

---
+++ Infrastructure.NHibernate/Audit/AuditRevisionEntity .cs
@@ -0,0 +1,13 @@
+﻿using NHibernate.Envers;
+using NHibernate.Envers.Configuration.Attributes;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo.Domain.AggregatesModel.Company2Aggregate
+{
+    [RevisionEntity]
+    [Table("REVINFO")]
+    public class AuditRevisionEntity : DefaultRevisionEntity
+    {
+        //Intentionally left blank
+    }
+}
