7f925bcac18287141af410bcff63aa8e449d168d
Mateusz <mateusz.wolnica@gmail.com>
2026-03-30 19:02:37 +0200

    .

---
+++ API.SlowTrainMachineLearning.UTs/MSSQL_Connections.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace API.SlowTrainMachineLearning.UTs
+{
+    public class MSSQL_Connections
+    {
+        [Fact]
+        public void CheckConnection()
+        {
+            var connectionString =
+            "Data Source=localhost,1500;" +
+            "User ID=sa;" +
+            "Password=Password_123#;" +
+            "Encrypt=True;" +
+            "TrustServerCertificate=True;";
+
+            try
+            {
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                Console.WriteLine("Połączenie działa!");
+
+                using var command = new SqlCommand("SELECT @@VERSION", connection);
+                var version = command.ExecuteScalar();
+
+                Console.WriteLine("Wersja SQL Server:");
+                Console.WriteLine(version);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Błąd połączenia:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
